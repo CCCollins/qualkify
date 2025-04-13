0eeeefa9a1674b5b4c2fa3bdb49fd9e59c3ac3f2
CCCollins <charlescollins1295001@gmail.com>
2025-04-13 08:21:39 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -139,6 +139,27 @@ export default function RootLayout({
             }
           `}
         </script>
+
+        <script type="text/javascript">
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(101018553, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true
+            });
+          `}
+        </script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/101018553" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
       </head>
       <body className="bg-gradient-to-br from-blue-100 via-sky-200 to-indigo-300 text-gray-900 min-h-screen flex items-center justify-center">
         <div className="absolute top-4 left-4 z-50">
