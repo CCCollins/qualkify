03c4bfe38b8da1f4047642f3355cd13ecbbde021
CCCollins <charlescollins1295001@gmail.com>
2025-04-13 15:29:40 +0300

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://qualkify.vercel.app', // Укажите базовый URL вашего сайта
+    generateRobotsTxt: true, // Генерация robots.txt
+    sitemapSize: 7000, // Максимальное количество URL в одном файле
+  };
